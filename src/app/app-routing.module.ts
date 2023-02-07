5c1f85e1f35f7caf5dc0329d8029c6a3c9f2d2fa
BuildTools <unconfigured@null.spigotmc.org>
2023-02-07 22:45:06 +0100

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component:ListComponent},
+  {path:'newMovies/:_id' , component:DetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
